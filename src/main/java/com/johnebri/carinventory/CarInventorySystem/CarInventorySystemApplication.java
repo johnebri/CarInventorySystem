a6273f6bb3e3281f95da4a2284776a4f8b4bdaad
johnebri <john.ebri@yahoo.com>
2020-06-11 10:16:08 +0100

    chore(app):app setup

---
+++ src/main/java/com/johnebri/carinventory/CarInventorySystem/CarInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.johnebri.carinventory.CarInventorySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnebri/carinventory/CarInventorySystem/CarInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnebri.carinventory.CarInventorySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
